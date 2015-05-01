dbd0bac30674810456fb7f5aa46ba76b273cebfb
Tomohiro Oda <tomohiro.tomo.oda@gmail.com>
2015-05-01 10:15:28 +0900

    added decorators to embed VDM-SL specs into python classes. added fibonacci example for decorators

---
+++ examples/fibonacci.py
@@ -0,0 +1,33 @@
+from pyVDMC import *
+
+@vdm_module('n1', 'n2')
+class fibonacci:
+    """
+    state State of 
+        n1 : nat
+        n2 : nat
+        init s == s = mk_State(0, 1)
+    end
+    operations
+        next : () ==> nat
+        next() == (dcl n : nat := n1 + n2; n1 := n2; n2 := n; return n)
+        post RESULT = n1~ + n2~ and n1 = n2~ and n2 = RESULT;
+        prev : () ==> nat
+        prev() == (dcl n : nat := n2 - n1; n2 := n1; n1 := n; return n2)
+        post n1 + n2 = n2~ and n2 = n1~;
+    """
+    def __init__(self):
+        self.n1 = 0
+        self.n2 = 1
+    @vdm_method
+    def next(self):
+        pass
+    @vdm_method
+    def next(self):
+        pass
+    @vdm_test
+    def prev(self):
+        n = self.n2 - self.n1
+        self.n2 = self.n1
+        self.n1 = n
+        return self.n2
